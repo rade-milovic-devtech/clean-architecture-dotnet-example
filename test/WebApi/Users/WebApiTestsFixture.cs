0b58d43d560db058a41085fce5bf34692d843dc9
Rade Milovic <rade.milovic@devtechgroup.com>
2018-09-09 11:41:10 +0200

    Simplifies the Web API integration tests fixture setup by using WebApplicationFactory.

--- test/WebApi/Users/WebApiTestsFixture.cs
+++ test/WebApi/Users/WebApiTestsFixture.cs
@@ -1,42 +1,35 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.TestHost;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.DependencyInjection;
 using Office365.UserManagement.Core.Users;
 using Office365.UserManagement.WebApi.Configuration;
-using System;
 using System.Net.Http;
 
 namespace Office365.UserManagement.WebApi.Users
 {
-	public class WebApiTestsFixture : IDisposable
+	public class WebApiTestsFixture : WebApplicationFactory<Startup>
 	{
-		private readonly TestServer server;
-
 		public WebApiTestsFixture()
 		{
-			var builder = new WebHostBuilder()
-				.ConfigureServices(services =>
-				{
-					var userDetailsPresenter = new UserDetailsPresenterStub();
-					UserOperations = new UserOperationsSimulator(userDetailsPresenter);
+			HttpClient = CreateDefaultClient();
+		}
 
-					services.AddScoped<UserDetailsPresenter>(provider => userDetailsPresenter);
-					services.AddScoped<IPerformUserOperations>(provider => UserOperations);
-				})
-				.UseStartup<Startup>();
-			server = new TestServer(builder);
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices(services =>
+			{
+				var userDetailsPresenter = new UserDetailsPresenterStub();
+				UserOperations = new UserOperationsSimulator(userDetailsPresenter);
 
-			HttpClient = server.CreateClient();
+				services.AddScoped<UserDetailsPresenter>(provider => userDetailsPresenter);
+				services.AddScoped<IPerformUserOperations>(provider => UserOperations);
+			});
+
+			base.ConfigureWebHost(builder);
 		}
 
 		public HttpClient HttpClient { get; }
 
 		public UserOperationsSimulator UserOperations { get; private set; }
-
-		public void Dispose()
-		{
-			HttpClient?.Dispose();
-			server?.Dispose();
-		}
 	}
 }
